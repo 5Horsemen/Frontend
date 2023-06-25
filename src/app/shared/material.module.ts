9152fc662c3ea940e5b99bc48835a4a3af2d888d
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2023-06-25 12:26:11 -0500

    initial commit

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,76 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@NgModule({
+    imports: [
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatFormFieldModule,
+        MatCardModule,
+        MatBadgeModule,
+        MatTabsModule,
+        MatButtonToggleModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatIconModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSnackBarModule,
+        MatListModule,
+        MatGridListModule,
+        MatTooltipModule,
+        MatMenuModule,
+
+
+    ],
+    exports: [
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatFormFieldModule,
+        MatCardModule,
+        MatBadgeModule,
+        MatTabsModule,
+        MatButtonToggleModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatIconModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSnackBarModule,
+        MatListModule,
+        MatGridListModule,
+        MatTooltipModule,
+        MatMenuModule,
+
+    ],
+
+})
+export class MaterialModule { }
