1185bc62874b4fff3533f0736874542f9170b571
RicharRomero123 <89022073+RicharRomero123@users.noreply.github.com>
2023-06-25 13:11:13 -0500

    feat: authServices, models

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {AuthService} from "../../services/auth/auth.service";
+import {AuthenticationRequestDto} from "../../models/auth/authentication-request-dto.model";
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login(): void {
+    const request: AuthenticationRequestDto = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.authService.login(request).subscribe(
+      () => {
+        // El inicio de sesión fue exitoso, redirigir a "/messages"
+        this.router.navigate(['/publications']);
+      },
+      (error) => {
+        // El inicio de sesión falló, puedes mostrar un mensaje de error al usuario
+        console.log('Error al iniciar sesión', error);
+      }
+    );
+  }
+}
+
