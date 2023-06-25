fa21d0d3ce9235ef8c433e96ab8b300c120d9956
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2023-06-25 16:15:33 -0500

    Bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { InboxComponent } from './views/inbox/inbox.component';
+import { PublicationsComponent } from './components/post/publications/publications.component';
+import { LayoutComponent } from './layout/layout.component';
+import { RegisterEmployerComponent } from './views/registers/register-employer/register-employer.component';
+
+
 const routes: Routes = [
 
   {
@@ -20,11 +26,24 @@ const routes: Routes = [
   {
     path: 'profile/:id',
     component: ProfileComponent,
+  },
+  {
+    path: 'accounts/register/employee',
+    component: RegisterEmployerComponent,
+  },
+  {
+    path: 'home',
+    component: LayoutComponent,
+  },
+  {
+    path: 'publications',
+    component: PublicationsComponent,
+  },
+  {
+    path: 'messages',
+    component: InboxComponent,
   }
-
 ];
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
