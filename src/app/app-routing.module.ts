1d8a4e1656cd4af9da7b434070c2b5da6f2dd6d6
Alonso Moisés García Murga <u202110867@upc.edu.pe>
2023-06-25 14:58:59 -0500

    fix: Profile Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 const routes: Routes = [
 
   {
@@ -12,6 +13,14 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent,
   },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'profile/:id',
+    component: ProfileComponent,
+  }
 
 ];
 
