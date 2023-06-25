284298c70d60f11c979c386eb18ea46295093427
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2023-06-25 18:26:04 -0500

    bug: fix

--- src/app/services/private-chat/private-chat.service.ts
+++ src/app/services/private-chat/private-chat.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CreatePrivateChatDto } from 'src/app/models/chat/create-private-chat-dto';
 import { PrivateChatDto } from 'src/app/models/chat/private-chat-dto.model';
@@ -14,20 +14,29 @@ export class PrivateChatService {
 
   constructor(private http: HttpClient) { }
 
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('access_token');
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', 'Bearer ' + token);
+
+    return headers;
+  }
+
+
   createPrivateChat(createChatRequest: CreatePrivateChatDto): Observable<PrivateChatDto> {
     return this.http.post<PrivateChatDto>(
-      `${this.baseUrl}`, createChatRequest);
+      `${this.baseUrl}`, createChatRequest, { headers: this.getHeaders() });
   }
   getPrivateChatByUsersId(user1Id: number, user2Id: number): Observable<PrivateChatDto> {
-    return this.http.get<PrivateChatDto>(`${this.baseUrl}/${user1Id}/${user2Id}`);
+    return this.http.get<PrivateChatDto>(`${this.baseUrl}/${user1Id}/${user2Id}`, { headers: this.getHeaders() });
   }
 
   getPrivateChatByUser1orUser2(user1Id: number, user2Id: number): Observable<PrivateChatDto[]> {
-    return this.http.get<PrivateChatDto[]>(`${this.baseUrl}/${user1Id}/or/${user2Id}`);
+    return this.http.get<PrivateChatDto[]>(`${this.baseUrl}/${user1Id}/or/${user2Id}`, { headers: this.getHeaders() });
   }
 
   getChatsByUserId(userId: number): Observable<PrivateChatDto[]> {
-    return this.http.get<PrivateChatDto[]>(`${this.baseUrl}/user/${userId}`);
+    return this.http.get<PrivateChatDto[]>(`${this.baseUrl}/user/${userId}`, { headers: this.getHeaders() });
   }
 
 }
